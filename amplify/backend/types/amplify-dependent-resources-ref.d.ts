17d39321cd516357ed55aca8caf1394dcb7d4bd4
thu yein aung <thu yein aung>
2022-03-07 01:44:47 +0900

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,23 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "yttest": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        },
+        "userPoolGroups": {
+            "leadersGroupRole": "string"
+        }
+    },
+    "api": {
+        "yttest": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,56 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <authenticator>
+    <template v-slot="{ user, signOut }">
+      <h3>{{ user.attributes.nickname }}</h3>
+      <p v-if="user.attributes.nickname == 'dododada'">This is dododada</p>
+      <button @click="createAuthor">Create Authors</button>
+      {{authors}}
+      <button @click="queryAuthors">Query Authors</button>
+      <div v-for="author in authors" :key=author.id>
+        <h1>
+          {{author.last_name}}
+        </h1>
+        <h5>
+          {{author.first_name}}
+        </h5>
+      </div>
+      <button @click="signOut">Sign Out</button>
+      </template>
+      </authenticator>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import {Authenticator} from "@aws-amplify/ui-vue";
+import { DataStore } from '@aws-amplify/datastore'
+import { Authors } from '../models';
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    Authenticator
+  },
+  data() {
+    return {
+      authors: []
+    }
+  },
+  methods: {
+    async createAuthor() {
+      await DataStore.save(
+        new Authors({
+          "first_name": "Lorem",
+          "last_name": "Episum",
+          "Posts": []
+        })
+      );
+    },
+    async queryAuthors() {
+      const models = await DataStore.query(Authors);
+      this.authors = models;
+      console.log(models);
+    }, 
   }
 }
 </script>
